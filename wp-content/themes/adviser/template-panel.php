b9d14f11b86edb512d1de8878d120d21306ee28e
Gianluca Marzano <gianlucamarzanogm@gmail.com>
2017-09-08 14:20:29 +0200

    Little fix to panel template

--- wp-content/themes/adviser/template-panel.php
+++ wp-content/themes/adviser/template-panel.php
@@ -1,9 +1,9 @@
 <?php
-/*
+/**
  * Template Name: Panel
  */
-
-get_header(); ?>
+?>
+<?php get_header();?>
 <section id="primary" class="content-area">
     <div id="content" class="site-content" role="main">
 
@@ -21,18 +21,22 @@ get_header(); ?>
                             bcn_display();
                         }?>
                     </div>
-					<div class="image-feature">
+                    <div class="image-feature">
                         <?php
                         if ( has_post_thumbnail() ) {
                             the_post_thumbnail();
                         }?>
                     </div>
-                    <?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
-                    <div class="page-header">
-                        <h1 class="page-title"><?php the_title();?></h1>
-                        <div class="page-content panel-template"><?php the_content();?></div>
-                    </div><!-- .page-header -->
-                    <?php endwhile;endif;?>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="list-content-page">
+                        <?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
+                            <div class="page-header">
+                            	<h1 class="page-title" style="display: none;"><?php the_title();?></h1>
+                            	<div class="page-description panel-template"><?php the_content();?></div>
+                            </div><!-- .page-header -->
+                        <?php endwhile;endif;?>
+                    </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-md-offset-1 col-sm-12 col-xs-12">
                     <?php get_sidebar('right');?>
@@ -42,5 +46,4 @@ get_header(); ?>
 
     </div><!-- #content -->
 </section><!-- #primary -->
-
 <?php get_footer();?>
