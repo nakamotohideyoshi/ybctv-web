c6da654409a0c7e0b28bbf9407ed706f93c4bd3c
Igor Pravdic <igorpravdic@gmail.com>
2017-09-29 16:33:02 +0200

    - IA update - Added panel template - css styling - added sponsored logo to best practice page - hidden view more btn form Latest news section (home page)

--- wp-content/themes/lw_international_adv/footer.php
+++ wp-content/themes/lw_international_adv/footer.php
@@ -19,7 +19,7 @@
                     <p><a href="http://www.expertinvestoreurope.com/gallery/1037738/-term-economic-trends-ignore" target="_blank" title="Six long-term economic trends you should not ignore">Six long-term economic trends you...</a></p>
                     <p><a href="http://www.expertinvestoreurope.com/news/1037687/inflows-stall-emerging-market-funds-2017" target="_blank" title="Inflows stall across emerging market funds in first for 2017">Inflows stall across emerging market...</a></p>
                     <p><a href="http://www.expertinvestoreurope.com/news/1037723/strong-em-passive-fund-inflows-pressure-active-fees" target="_blank" title="Strong EM passive fund inflows pressure active fees">Strong EM passive fund inflows pressure...</a></p>
-                    <p class="more_from"><a href="http://www.expertinvestoreurope.com" target="_blank">More News From EI</a></p>
+                    <p class="more_from"><a href="http://www.expertinvestoreurope.com" target="_blank">More news From EI</a></p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
@@ -30,7 +30,7 @@
                     <p><a href="http://www.fundselectorasia.com/gallery/1037736/head-head-vs-partners" target="_blank" title="HEAD-TO-HEAD: First State vs Partners Group">HEAD-TO-HEAD: First State vs Partners...</a></p>
                     <p><a href="http://www.fundselectorasia.com/news/1037737/missed" target="_blank" title="In case you missed it...">In case you missed it...</a></p>
                     <p><a href="http://www.fundselectorasia.com/news/1037735/report-hnwis-stand-em-investors" target="_blank" title="Report: HNWIs stand out as EM investors">Report: HNWIs stand out as EM investors...</a></p>
-                    <p class="more_from"><a href="http://www.fundselectorasia.com" target="_blank">More News From FSA</a></p>
+                    <p class="more_from"><a href="http://www.fundselectorasia.com" target="_blank">More news From FSA</a></p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
@@ -41,7 +41,7 @@
                     <p><a href="http://www.portfolio-adviser.com/news/1037929/woodford-apologises-incredibly-painful-underperformance" target="_blank" title="Woodford apologises for 'incredibly painful' underperformance">Woodford apologises for 'incredibly painful' un...</a></p>
                     <p><a href="http://www.portfolio-adviser.com/analysis/1037926/pa-analysis-hey-fund-selectors-wheres-value-add" target="_blank" title="PA ANALYSIS: Hey, fund selectors - where's the value add?">PA ANALYSIS: Hey, fund selectors...</a></p>
                     <p><a href="http://www.portfolio-adviser.com/news/1037924/jupiter-expands-gem-range-short-duration-bond-fund" target="_blank" title="Jupiter expands GEM range with short-duration bond fund">Jupiter expands GEM range...</a></p>
-                    <p class="more_from"><a href="http://international-adviser.com/" target="_blank">More News From PA</a></p>
+                    <p class="more_from"><a href="http://portfolio-adviser.com/" target="_blank">More news From PA</a></p>
                 </div>
             </div>
         </div>
@@ -51,7 +51,7 @@
 
 <footer id="colophon" class="site-footer" role="contentinfo">
     <div class="container">
-	    <div class="content-footer">
+        <div class="content-footer">
             <div class="row row-eq-height">
                 <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
                     <div class="menu-footer clearfix">
@@ -63,7 +63,7 @@
                     <div class="footer_connect">
                     <h2>International Adviser</h2>
                     <p>Published by Last Word Media (UK) Limited, Fleet House, 1st Floor, 59-61 Clerkenwell Road, London, EC1M 5LA. Copyright (c) <?php echo date('Y'); ?>. All rights reserved. Company Reg. No. 05573633. VAT. No. 872 411 728.&nbsp;ISSN 2397-284X</p>
-                    <p>International Adviser is a monthly news magazine and daily news web site aimed at wealth managers , investment IFAs and other professional fund pickers and asset allocators in the UK and Channel Islands. No news, articles or content may be reproduced in part or in full without express permission of International Adviser.</p>
+                    <p>International Adviser is a monthly digital magazine and daily news website aimed at wealth managers, investment IFAs and other professional fund pickers and asset allocators in the UK and Channel Islands. No news, articles or content may be reproduced in part or in full without express permission of International Adviser.</p>
                     <!---->
                 </div>
                 </div>
@@ -73,7 +73,7 @@
                     </div>
                 </div>
             </div>
-	    </div>
+        </div>
     </div>
 </footer><!-- #colophon -->
 
@@ -83,57 +83,57 @@
 <?php /*
 <script>
 
-	var form = jQuery("#example-advanced-form").show();
+    var form = jQuery("#example-advanced-form").show();
  
-	form.steps({
-		headerTag: "h3",
-		bodyTag: "fieldset",
-		transitionEffect: "slideLeft",
-		onStepChanging: function (event, currentIndex, newIndex)
-		{
-			// Allways allow previous action even if the current form is not valid!
-			if (currentIndex > newIndex)
-			{
-				return true;
-			}
-			// Forbid next action on "Warning" step if the user is to young
-			if (newIndex === 3 && Number(jQuery("#age-2").val()) < 18)
-			{
-				return false;
-			}
-			// Needed in some cases if the user went back (clean up)
-			if (currentIndex < newIndex)
-			{
-				// To remove error styles
-				form.find(".body:eq(" + newIndex + ") label.error").remove();
-				form.find(".body:eq(" + newIndex + ") .error").removeClass("error");
-			}
-			form.validate().settings.ignore = ":disabled,:hidden";
-			return form.valid();
-		},
-		onStepChanged: function (event, currentIndex, priorIndex)
-		{
-			// Used to skip the "Warning" step if the user is old enough.
-			if (currentIndex === 2 && Number(jQuery("#age-2").val()) >= 18)
-			{
-				form.steps("next");
-			}
-			// Used to skip the "Warning" step if the user is old enough and wants to the previous step.
-			if (currentIndex === 2 && priorIndex === 3)
-			{
-				form.steps("previous");
-			}
-		},
-		onFinishing: function (event, currentIndex)
-		{
-			form.validate().settings.ignore = ":disabled";
-			return form.valid();
-		},
-		onFinished: function (event, currentIndex)
-		{
-			alert("Submitted!");
-		}
-	});
+    form.steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "slideLeft",
+        onStepChanging: function (event, currentIndex, newIndex)
+        {
+            // Allways allow previous action even if the current form is not valid!
+            if (currentIndex > newIndex)
+            {
+                return true;
+            }
+            // Forbid next action on "Warning" step if the user is to young
+            if (newIndex === 3 && Number(jQuery("#age-2").val()) < 18)
+            {
+                return false;
+            }
+            // Needed in some cases if the user went back (clean up)
+            if (currentIndex < newIndex)
+            {
+                // To remove error styles
+                form.find(".body:eq(" + newIndex + ") label.error").remove();
+                form.find(".body:eq(" + newIndex + ") .error").removeClass("error");
+            }
+            form.validate().settings.ignore = ":disabled,:hidden";
+            return form.valid();
+        },
+        onStepChanged: function (event, currentIndex, priorIndex)
+        {
+            // Used to skip the "Warning" step if the user is old enough.
+            if (currentIndex === 2 && Number(jQuery("#age-2").val()) >= 18)
+            {
+                form.steps("next");
+            }
+            // Used to skip the "Warning" step if the user is old enough and wants to the previous step.
+            if (currentIndex === 2 && priorIndex === 3)
+            {
+                form.steps("previous");
+            }
+        },
+        onFinishing: function (event, currentIndex)
+        {
+            form.validate().settings.ignore = ":disabled";
+            return form.valid();
+        },
+        onFinished: function (event, currentIndex)
+        {
+            alert("Submitted!");
+        }
+    });
 
 </script>
 */ ?>
---
+++ wp-content/themes/lw_international_adv/template-panel.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Template Name: Panel
+ */
+?>
+<?php get_header();?>
+<section id="primary" class="content-area">
+    <div id="content" class="site-content" role="main">
+
+        <div class="content-page">
+            <div class="container">
+                <div class="col-lg-2 col-md-2 col-sm-12 col-xs-12">
+                    <div class="content-left">
+                        <?php get_sidebar('left');?>
+                    </div>
+                </div>
+                <div class="col-lg-10 col-md-10 col-sm-12 col-xs-12">
+                    <div class="bread">
+                        <?php if(function_exists('bcn_display'))
+                        {
+                            bcn_display();
+                        }?>
+                    </div>
+                    <div class="image-feature">
+                        <?php
+                        if ( has_post_thumbnail() ) {
+                            the_post_thumbnail();
+                        }?>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="list-content-page">
+                        <?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
+                            <div class="page-header">
+                            	<h1 class="page-title" style="display: none;"><?php the_title();?></h1>
+                            	<div class="page-description panel-template"><?php the_content();?></div>
+                            </div><!-- .page-header -->
+                        <?php endwhile;endif;?>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-md-offset-1 col-sm-12 col-xs-12">
+                    <?php get_sidebar('right');?>
+                </div>
+            </div>
+        </div>
+
+    </div><!-- #content -->
+</section><!-- #primary -->
+<?php get_footer();?>
