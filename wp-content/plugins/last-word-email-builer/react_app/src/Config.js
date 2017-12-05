307e8d7a0c06762ea47e1c36229abad00a365146
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2017-12-05 09:30:00 +0200

    Featured articles

--- wp-content/plugins/last-word-email-builer/react_app/src/Config.js
+++ wp-content/plugins/last-word-email-builer/react_app/src/Config.js
@@ -1,6 +1,6 @@
 const Config = {
   BASE_URL: window.location.host.indexOf('localhost') != -1 ? 'https://lastword.staging.wpengine.com' : (window.location.protocol + "//" + window.location.host),
-  VERSION: 2
+  VERSION: 1
 };
 
 console.log(Config.BASE_URL);
