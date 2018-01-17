2f65674adb596f75315e71aba3e1aa7a36308f9f
nakamoto hideyoshi <nakamoto.guru.0991@gmail.com>
2018-01-17 13:05:47 +0200

    Preview box small fixes

--- wp-content/plugins/last-word-email-builer/react_app/src/PreviewBox.js
+++ wp-content/plugins/last-word-email-builer/react_app/src/PreviewBox.js
@@ -8,7 +8,7 @@ class PreviewBox extends Component {
 
   render() {
     return (
-     	<div style={{ 'display': this.props.show ? 'block' : 'none', 'position': 'fixed', 'left': 20, 'top': 20, 'border': '1px solid #000', 'background': '#fff', 'zIndex': 10, 'minWidth': 400, 'maxWidth': 800, 'minHeight': 100 }}>
+     	<div style={{ 'display': this.props.show ? 'block' : 'none', 'position': 'fixed', 'left': 200, 'top': 50, 'border': '1px solid #000', 'background': '#fff', 'zIndex': 20, 'minWidth': 400, 'maxWidth': 800, 'minHeight': 100 }}>
 			<div dangerouslySetInnerHTML={{__html: this.props.content }}></div>
 	 	</div>
     );
